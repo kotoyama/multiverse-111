203a2ad124ee158b1f23859edd66b741ef8a5f64
kotoyama <snrsrrw@pm.me>
2025-05-25 16:43:03 +0300

    display user data

--- apps/cosmoswin/app/page.tsx
+++ apps/cosmoswin/app/page.tsx
@@ -1,15 +1,5 @@
-import { getTranslations } from '@repo/i18n/server'
-
-import { appName } from './app-name'
-import styles from './page.module.css'
+import { UserInfo } from './user-info'
 
 export default async function HomePage() {
-  const t = await getTranslations('homePage')
-
-  return (
-    <main>
-      <h1 className={styles.title}>{t('title', { brand: appName })}</h1>
-      <p>{t('subtitle')}</p>
-    </main>
-  )
+  return <UserInfo />
 }
